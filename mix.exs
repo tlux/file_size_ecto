979fecdf39834ee94109fe878d6a852b57923537
Tobias Casper <tobias.casper@gmail.com>
2020-04-23 22:20:16 +0200

    fix: loosen file_size dependency

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule FileSize.Ecto.MixProject do
   def project do
     [
       app: :file_size_ecto,
-      version: "2.0.0",
+      version: "2.0.1",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       description: description(),
@@ -52,11 +52,11 @@ defmodule FileSize.Ecto.MixProject do
   defp deps do
     [
       {:credo, "~> 1.0.5", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.0.0-rc.6", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.0.0", only: [:dev, :test], runtime: false},
       {:ecto, "~> 3.0"},
       {:ex_doc, "~> 0.20.2", only: :dev, runtime: false},
       {:excoveralls, "~> 0.11.0", only: :test},
-      {:file_size, "~> 2.0"}
+      {:file_size, ">= 2.0.0 and < 4.0.0"}
     ]
   end
 
