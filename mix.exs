d483288689fb442ea68d1e75674860a26c75f14d
Tobias Casper <tobias.casper@gmail.com>
2020-04-23 22:24:37 +0200

    refactor: remove logger from extra apps

--- mix.exs
+++ mix.exs
@@ -44,7 +44,7 @@ defmodule FileSize.Ecto.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: []
     ]
   end
 
